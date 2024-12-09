a9c05d570cfafd57fb9296fc68cdbfee92aa159c
AderKonstantin <aderkonstantin9@gmail.com>
2024-12-09 13:54:52 +0300

    many bullshit updates
    models:
    1) BuilderBlockout
    2) TrooperModel
    UI
    3) ArmorBar
    4) Healthbar
    5) Fonts (RobotoMono)
    6) Icons
    7) Build Profiles
    8) Tags for Camera

---
+++ Assets/Scripts/Runtime/Core/PlayerCamera.cs
@@ -0,0 +1,12 @@
+using Unity.Netcode;
+using UnityEngine;
+
+public class PlayerCamera : NetworkBehaviour
+{
+    private Camera playerCamera;
+
+    void Start()
+    {
+        // Something will be here
+    }
+}
