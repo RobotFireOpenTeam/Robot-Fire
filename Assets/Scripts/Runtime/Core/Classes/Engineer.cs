ebf5ef05e5a1b4cb9fcd81b2351083b81ad0e9f5
AderKonstantin <aderkonstantin9@gmail.com>
2024-12-10 03:28:50 +0300

    style: change all name variables to code style convention

--- Assets/Scripts/Runtime/Core/Classes/Engineer.cs
+++ Assets/Scripts/Runtime/Core/Classes/Engineer.cs
@@ -2,15 +2,5 @@ using UnityEngine;
 
 public class Engineer : RobotController
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    // Engineer Class
 }
